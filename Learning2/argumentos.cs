214424a7ecb795c7e115f18f31400ea1b9b5705c
OFelipeSoares <felipebusiness.soares@gmail.com>
2023-02-19 08:53:00 -0300

    19/02/2023

---
+++ Learning2/argumentos.cs
@@ -0,0 +1,10 @@
+using System;
+
+class sistema
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Seja bem vindo, {0}!", args[0]);
+        Consolw.WriteLine("Esse é o curso de C# Por Cralos");
+    }
+}
