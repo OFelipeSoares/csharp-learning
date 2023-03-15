4c4703b76f1a8d74e34bd045c860d2db6dfb3501
OFelipeSoares <felipebusiness.soares@gmail.com>
2023-03-15 07:08:13 -0300

    Att

---
+++ Estruturas de Repeti;'ao/Estruturas de Repeti;'ao/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Estruturas_de_Repeti__ao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputValor = new System.Windows.Forms.TextBox();
+            this.InputTempo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputValor
+            // 
+            this.inputValor.Location = new System.Drawing.Point(196, 60);
+            this.inputValor.Name = "inputValor";
+            this.inputValor.Size = new System.Drawing.Size(195, 22);
+            this.inputValor.TabIndex = 0;
+            this.inputValor.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // InputTempo
+            // 
+            this.InputTempo.Location = new System.Drawing.Point(196, 103);
+            this.InputTempo.Name = "InputTempo";
+            this.InputTempo.Size = new System.Drawing.Size(195, 22);
+            this.InputTempo.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Valor Investido";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tempo Investido (meses)";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(206, 158);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 45);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Calcular Rendimento";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(793, 531);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.InputTempo);
+            this.Controls.Add(this.inputValor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputValor;
+        private System.Windows.Forms.TextBox InputTempo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Estruturas de Repeti;'ao/Estruturas de Repeti;'ao/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Estruturas_de_Repeti__ao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int total = 2;
+            for (int i = 0; i <5 ; i++)
+            {
+                total = total * 2;
+            }
+            MessageBox.Show("O total é " + total);
+            /*
+            double valorInvestido = Convert.ToDouble(inputValor.Text);
+            int tempoInvestido = Convert.ToInt32(InputTempo.Text);
+
+            for (int i = 0; i < tempoInvestido; i++)
+            {
+                valorInvestido = valorInvestido * 1.01;
+            }
+            int i = 1; 
+            
+             while (i <= tempoInvestido) ;
+            {
+                valorInvestido = valorInvestido * 1.01;
+                i++;
+            }
+            
+
+            MessageBox.Show($"O valor investido ao final dos {InputTempo} meses é {valorInvestido}");
+            */
+        }
+    }
+}
